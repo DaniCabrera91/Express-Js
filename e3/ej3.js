a52a22f8623c42135060ad3e11934e63e690edfb
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-06-12 03:18:06 +0200

    progress in the 3rth exercice creating the endpoints

--- e3/ej3.js
+++ e3/ej3.js
@@ -1,3 +1,5 @@
+// // Crear un archivo con el nombre ej3.js
+// // Levantar un servidor de Express
 const express = require('express')
 const puerto = 3000
 const app = express()
@@ -6,31 +8,71 @@ app.listen(puerto, () => {
  console.log(`Servidor levantado en el puerto ${puerto}`)
 })
 
+const productos = {
+    description: 'Productos',
+   items: [
+     { id: 1, nombre: 'Taza de Harry Potter' , precio: 300},
+     { id: 2, nombre: 'FIFA 22 PS5' , precio: 1000},
+     {  id: 3, nombre: 'Figura Goku Super Saiyan' , precio: 100},
+     {  id: 4,  nombre: 'Zelda Breath of the Wild' , precio: 200},
+     {  id: 5,  nombre: 'Skin Valorant' , precio: 120},
+     {  id: 6, nombre: 'Taza de Star Wars' , precio: 220}
+   ]
+ }
+
+const productsItems = productos.items;  
+app.use(express.json())
+
+// Al llamar a localhost:3000/products se debe mostrar el siguiente JSON:
 app.get('/products', (req, res) => {
-    res.send({
-         description: 'Productos',
-        items: [
-          { id: 1, nombre: 'Taza de Harry Potter' , precio: 300},
-          { id: 2, nombre: 'FIFA 22 PS5' , precio: 1000},
-          {  id: 3, nombre: 'Figura Goku Super Saiyan' , precio: 100},
-          {  id: 4,  nombre: 'Zelda Breath of the Wild' , precio: 200},
-          {  id: 5,  nombre: 'Skin Valorant' , precio: 120},
-          {  id: 6, nombre: 'Taza de Star Wars' , precio: 220}
-        ]
-      })
+    res.status(200).send(productos);
+});
+
+// Crear endpoint para poder crear un producto nuevo
+app.post('/products', (req, res) => {
+    console.log(productsItems);
+    const {nombre, precio} = req.body
+    const newProduct = {
+        id: productsItems.length +1,
+        nombre,
+        precio
+    }
+    if (!nombre || !precio){
+        res.status(400).send({message: "rellena todos los campos"})
+    }else{
+        productsItems.push(newProduct)
+        res.status(200).send(productos)
+    }
 })
-// const productos = [
-//     {
-//          description: 'Productos',
-//         items: [
-//           { id: 1, nombre: 'Taza de Harry Potter' , precio: 300},
-//           { id: 2, nombre: 'FIFA 22 PS5' , precio: 1000},
-//           {  id: 3, nombre: 'Figura Goku Super Saiyan' , precio: 100},
-//           {  id: 4,  nombre: 'Zelda Breath of the Wild' , precio: 200},
-//           {  id: 5,  nombre: 'Skin Valorant' , precio: 120},
-//           {  id: 6, nombre: 'Taza de Star Wars' , precio: 220}
-//         ]
-//       }
-      
-//    ]
-   
+
+// Crear endpoint para poder actualizar un producto
+app.put('/products/:id', (req, res) => {
+    const productId = parseInt(req.params.id, 10); // Convierte a número
+    const filterById = productsItems.some(item => item.id === +req.params.id)
+if (filterById){
+    productsItems.forEach((item) => {
+        if (item.id == productId) {
+            item.nombre = req.body.nombre ? req.body.nombre : item.nombre
+            item.precio = req.body.precio ? req.body.precio : item.precio
+            res.status(200).send(item)
+        }
+    })
+}else {
+
+res.status(400).send({message: 'error'})
+}    
+})
+
+// Crear endpoint para poder eliminar un producto
+app.delete('/products/id/:id', (req, res) => {
+    const result = productsItems.some(item => item.id === +req.params.id)
+    res.status(200).send(result);
+})
+
+// Crear filtro por precio de producto
+
+// Crear filtro que muestre los productos con un precio entre 50 y 250.
+
+// Crear un filtro que cuando busque en postman por parámetro el id de un producto me devuelva ese producto
+
+// Crear un filtro que cuando busque en postman por parámetro el nombre de un producto me devuelva ese producto
